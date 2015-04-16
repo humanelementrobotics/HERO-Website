53f5e85a74b26b2370d7e895b385736fc723e50b
ErikMLutz <erikmclutz@gmail.com>
2015-04-16 19:49:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-HERO!
-Developing robots to advance humanity
-  -Zack Allen
-  -Brendan Boyle
-  -Erik Lutz
+<h1>HERO!</h1><br>
+Developing robots to advance humanity<br>
+<ul>
+  <li>Zack Allen</li>
+  <li>Brendan Boyle</li>
+  <li>Erik Lutz</li>
+</ul>
